94880314b7b61b6ca5a28e8498f551c17be8911b
LeeHuSeong <84837365+leehuseong@users.noreply.github.com>
2025-08-01 09:21:42 +0900

    main view수정

--- MediCon/src/main/java/com/medicon/medicon/Main.java
+++ MediCon/src/main/java/com/medicon/medicon/Main.java
@@ -8,8 +8,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        //Main_view
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+//                "/com/medicon/medicon/view/medic/medic_main/Main.fxml"
+//        ));
+        //login_view
         FXMLLoader loader = new FXMLLoader(getClass().getResource(
-                "/com/medicon/medicon/view/medic/medic_main/Main.fxml"
+                "/com/medicon/medicon/view/login.fxml"
         ));
         Scene scene = new Scene(loader.load());
         stage.setTitle("MediCon 로그인");
